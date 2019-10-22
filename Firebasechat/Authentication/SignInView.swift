a80cda220c97c7635869947a8baa92b03d2fa24c
Enrique Mosqueda <Wicky@Enriques-MacBook-Pro-2.local>
2019-10-21 17:36:07 -0700

    added stuff for firebase

--- Firebasechat/Authentication/SignInView.swift
+++ Firebasechat/Authentication/SignInView.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
+import Firebase
+
 
 class SignInView: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginAction(_ sender: Any) {
+        guard let email = emailField.text else { return }
+        guard let password = passwordField.text else { return }
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if error == nil {
+                print("success")
+                self.performSegue(withIdentifier: "toHome", sender: nil)
+            } else {
+                print("error")
+            }
+        }
     }
-    */
-
 }
